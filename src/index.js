474c42079c69821c69cfb13996389eb4c07de845
dariusalba <75644974+Dariusalba@users.noreply.github.com>
2023-10-27 10:29:30 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const database = require('./config/database')
+const eventRoutes = require('/routes/EventRoutes')
+
+const app = express();
+app.use(express.json());
+app.use('./events', eventRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
