fd600906fa8d5a39ba2b4615fb766e1da0174256
dariusalba <paul.darius34@yahoo.com>
2024-01-11 17:16:26 +0200

    added event deletion, event participation

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -5,5 +5,7 @@ const eventController = require('../controllers/eventController');
 router.get('/get-all', eventController.getEvents);
 router.post('/create-event', eventController.createEvent);
 router.post('/confirmation/:eventId/:userMail', eventController.confirmation);
+router.post('/attend/:eventId/:userMail', eventController.attend);
+router.delete('/delete-event/:eventId', eventController.deleteEvent);
 
 module.exports = router;
